a80fdc2dbd78aa3fbe72d399f885087a626b554a
Tas Yusoontorn <mac@Macs-MacBook-Air-8.local>
2017-08-15 14:50:32 +0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -50,8 +50,25 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
-  })
-;};
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
