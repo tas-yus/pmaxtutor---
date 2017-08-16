d6491bfd96f82f1112a7bda7c835696fab6e50c7
Tas Yusoontorn <mac@Macs-MacBook-Air-8.local>
2017-08-16 14:55:11 +0700

    Add tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,46 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+const {Course} = require('./../../models/course');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const courses = [{
+  _id: new ObjectID,
+  name: 'chemO'
+}, {
+  _id: new ObjectID,
+  name: 'Bruh',
+  completed: true,
+  completed: 333
+}];
+
+const users = [{
+  _id: userOneId,
+  email: 'tas@example.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'lol@s.com',
+  password: 'userTwoPass'
+}];
+
+const populateCourses = (done) => {
+  Course.remove({}).then(() => {
+    return Course.insertMany(courses);
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+  }).then(() => done());
+}
+
+module.exports = {courses, users, populateCourses, populateUsers};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,22 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Course} = require('./../models/course');
+const {User} = require('./../models/user');
+const {courses, users, populateUsers, populateCourses} = require('./seed/seed');
 
-const courses = [{
-  _id: new ObjectID,
-  name: 'chemO'
-}, {
-  _id: new ObjectID,
-  name: 'Bruh',
-  completed: true,
-  completed: 333
-}];
-
-beforeEach((done) => {
-  Course.remove({}).then(() => {
-    return Course.insertMany(courses);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateCourses);
 
 describe('POST /courses', () => {
   it('should add a new course', (done) => {
@@ -177,6 +166,80 @@ describe('PATCH /courses/:id', () => {
           expect(res.body.course.completed).toBe(false);
           expect(res.body.course.completedAt).toNotExist();
         })
-        .end(done)
-  })
+        .end(done);
+  });
+});
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return a 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    var email = 'lol@l.com'
+    var password = '123mbroe'
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findOne({email}).then((user) => {
+          expect(user.email).toBe(email);
+          expect(user.password).toNotBe(password);
+          done();
+        }).catch((e) => done());
+      });
+  });
+
+  it('should return validation errors if request invalid', (done) => {
+    var email = 'l';
+    var password = '123mbroe';
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use', (done) => {
+    var email = 'l';
+    var password = '123mbroe';
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: 'Password123!'
+      })
+      .expect(400)
+      .end(done);
+  });
 });
